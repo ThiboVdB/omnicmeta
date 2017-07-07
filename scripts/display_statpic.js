df4bf28b137b3b375745869b23b2b8bfb1a8cc03
theswitchfox <theswitchfox@gmail.com>
2017-07-07 10:07:39 -0400

    fixed urls

--- scripts/display_statpic.js
+++ scripts/display_statpic.js
@@ -15,11 +15,11 @@
     html_str += ' <a href=" http://www.omnicmeta.com/p/pc-us-damage.html ">Damage Done</a> &nbsp; &nbsp;';
     html_str += ' <a href=" http://www.omnicmeta.com/p/pc-us-blocked.html ">Damage Blocked</a> &nbsp; &nbsp;';
     html_str += ' </br> ';
-    html_str += ' <a href=" http://www.omnicmeta.com/p/pc-us-total_assists.html ">Total Assists</a> &nbsp; &nbsp;';    
-    html_str += ' <a href=" http://www.omnicmeta.com/p/pc-us-def_assists.html ">Def. Assists</a> &nbsp; &nbsp;';  
-    html_str += ' <a href=" http://www.omnicmeta.com/p/pc-us-off_assists.html ">Off. Assists</a> &nbsp; &nbsp;';  
+    html_str += ' <a href=" http://www.omnicmeta.com/p/pc-us-total-assists.html ">Total Assists</a> &nbsp; &nbsp;';    
+    html_str += ' <a href=" http://www.omnicmeta.com/p/pc-us-def-assists.html ">Def. Assists</a> &nbsp; &nbsp;';  
+    html_str += ' <a href=" http://www.omnicmeta.com/p/pc-us-off-assists.html ">Off. Assists</a> &nbsp; &nbsp;';  
     html_str += ' <a href=" http://www.omnicmeta.com/p/pc-us-accuracy.html ">Accuracy</a> &nbsp; &nbsp;';  
-    html_str += ' <a href=" http://www.omnicmeta.com/p/pc-us-crit_accuracy.html ">Crit Accuracy</a> &nbsp; &nbsp;';         
+    html_str += ' <a href=" http://www.omnicmeta.com/p/pc-us-crit-accuracy.html ">Crit Accuracy</a> &nbsp; &nbsp;';         
     html_str += ' </td></tr><tr>';
     html_str += ' <td style="text-align:center"> &nbsp; &nbsp; &nbsp; Console &nbsp; &nbsp; &nbsp;</td><td>';
     html_str += ' <a href=" http://www.omnicmeta.com/p/console-winrate.html ">Winrates</a> &nbsp; &nbsp;';
@@ -30,11 +30,11 @@
     html_str += ' <a href=" http://www.omnicmeta.com/p/console-damage.html ">Damage Done</a> &nbsp; &nbsp;';
     html_str += ' <a href=" http://www.omnicmeta.com/p/console-blocked.html ">Damage Blocked</a> &nbsp; &nbsp;';
     html_str += ' </br> ';
-    html_str += ' <a href=" http://www.omnicmeta.com/p/console-total_assists.html ">Total Assists</a> &nbsp; &nbsp;';    
-    html_str += ' <a href=" http://www.omnicmeta.com/p/console-def_assists.html ">Def. Assists</a> &nbsp; &nbsp;';  
-    html_str += ' <a href=" http://www.omnicmeta.com/p/console-off_assists.html ">Off. Assists</a> &nbsp; &nbsp;';  
+    html_str += ' <a href=" http://www.omnicmeta.com/p/console-total-assists.html ">Total Assists</a> &nbsp; &nbsp;';    
+    html_str += ' <a href=" http://www.omnicmeta.com/p/console-def-assists.html ">Def. Assists</a> &nbsp; &nbsp;';  
+    html_str += ' <a href=" http://www.omnicmeta.com/p/console-off-assists.html ">Off. Assists</a> &nbsp; &nbsp;';  
     html_str += ' <a href=" http://www.omnicmeta.com/p/console-accuracy.html ">Accuracy</a> &nbsp; &nbsp;';  
-    html_str += ' <a href=" http://www.omnicmeta.com/p/console-crit_accuracy.html ">Crit Accuracy</a> &nbsp; &nbsp;';           
+    html_str += ' <a href=" http://www.omnicmeta.com/p/console-crit-accuracy.html ">Crit Accuracy</a> &nbsp; &nbsp;';           
     html_str += ' </td></tr></table> <br/> <hr> <br>';
 
 
