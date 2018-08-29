ea52f9450ce26f5dcf119f615fcdf1b36a2c6f10
theswitchfox <theswitchfox@gmail.com>
2018-08-29 11:31:53 -0400

    added table loaded lock

--- scripts/percentile_tables_tiny_pq.js
+++ scripts/percentile_tables_tiny_pq.js
@@ -1,3 +1,4 @@
+table_loaded = true;
 var bucket_percentile = [1,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,99];
 function GetValueTier(tier, hero, stat, value, mode){
   var retval = 1; 
